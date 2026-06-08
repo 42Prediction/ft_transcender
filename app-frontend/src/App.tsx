675e15497f8763394ff79fd2557a3d3173bd8136
nfigueir <nfigueir@student.42luanda.com>
2026-06-08 15:54:15 +0100

    feat: atualiza a rota e adiciona navbar e hero

--- app-frontend/src/App.tsx
+++ app-frontend/src/App.tsx
@@ -1,53 +1,9 @@
-import Navbar from './features/public/components/Navbar.tsx';
-import { Home } from './features/public/pages/Home.tsx';
-import Notfound from './components/NotFound';
-import Footer from './components/Footer';
-import { Outlet, useNavigation, RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { PrivateRoute } from './components/PrivateRoute';
-import { LoginPage } from './features/auth/pages/LoginPage.tsx';
-import { AuthCallback } from './features/public/pages/AuthCallback.tsx';
-import { AuthProvider } from './context/AuthContext.tsx';
-import { profileRoute, protectedProfileRoute } from './features/profile/route.tsx';
-
-function ProtectedLayout() {
-  const navigation = useNavigation();
-
-  return (
-    <>
-      <Navbar />
-      {/* barra de loading no topo enquanto loader roda */}
-      {navigation.state === 'loading' && (
-        <div className="fixed top-0 left-0 h-0.5 w-full bg-primary z-50 animate-pulse" />
-      )}
-      <Outlet />
-      <Footer />
-    </>
-  );
-}
-
-const router = createBrowserRouter([
-  {path: 'login', element: <LoginPage />},
-  { path: 'auth/callback', element: <AuthCallback />},
-  ...profileRoute,
-  {
-    element: (
-      <PrivateRoute>
-        <ProtectedLayout />
-      </PrivateRoute>
-    ),
-    children:
-      [
-        { path: '/', element: <Home /> },
-        ...protectedProfileRoute
-      ]
-  },
-  { path: '*', element: <Notfound />}
-])
+import { Home } from "./features/public/pages/Home";
 
 export default function App() {
   return (
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <div>
+      <Home/>
+    </div>
   );
 }
--- app-frontend/src/features/public/pages/Home.tsx
+++ app-frontend/src/features/public/pages/Home.tsx
@@ -1,20 +1,11 @@
-import { useEffect, useState } from "react";
-import { useAuth } from "../../../context/AuthContext";
-import { bettor } from "../../../api/bettor/bettor.api";
+import { Hero } from "../components/Hero";
+import { Navbar } from "../components/Navbar";
 
 export function Home(){
-    const {user, logout } = useAuth();
-    const [profile, setProfile] = useState(null);
-
-    useEffect(() => {
-        bettor.getMe().then(({ data }) => setProfile(data));
-    },[]);
-
     return (
-    <div>
-        <h1>Olá, {user?.email}</h1>
-        <pre>{JSON.stringify(profile, null, 2)}</pre>
-        <button onClick={logout}>Sair</button>
-    </div>
+        <div className="dark min-h-screen bg-background text-foreground">
+            <Navbar />
+            <Hero/>
+        </div>
     );
 }
